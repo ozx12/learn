20081c10b6eacc9b3ac2ccade0f81ca733acbb06
ozx12 <sergey@butusov.su>
2022-05-26 16:34:34 +0300

    0.2

--- starting-template/src/App.vue
+++ starting-template/src/App.vue
@@ -11,7 +11,31 @@
             <spinner/>
 
             <div class="row">
-                <h2>Карточки персонажей...</h2>
+                    <div class="col-lg-4 col-md-6 col-sm-12">
+
+                <div class="card mb-3" style="max-width: 540px;">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img class="img-fluid rounded-start"
+                                src="http://i.annihil.us/u/prod/marvel/i/mg/3/50/526548a343e4b.jpg" alt="Человек-паук">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Человек-паук</h5>
+
+                                <button type="button" class="btn btn-primary" data-bs-toggle="modal"
+                                    data-bs-target="#exampleModal">
+                                    Подробнее
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+            </div>
             </div>
         </div>
 
--- starting-template/src/components/AppHeader.vue
+++ starting-template/src/components/AppHeader.vue
@@ -1,5 +1,17 @@
 <template>
-    <h2>AppHeader</h2>
+       <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Марвел</a>
+
+            <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+
+                <form class="d-flex justify-content-end" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Поиск" aria-label="Search">
+                    <button class="btn btn-primary" type="submit">Все</button>
+                </form>
+            </div>
+        </div>
+    </nav>
 </template>
 
 <script>
